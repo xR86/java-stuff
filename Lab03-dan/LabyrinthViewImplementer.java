8008b5c2495377e0d6d7d6c50c866e5163f48b24
Luca Iulian <iulian.luca@info.uaic.ro>
2016-03-18 09:03:00 +0200

    null pointer resolved. matrix shows fully

--- Lab03-dan/LabyrinthViewImplementer.java
+++ Lab03-dan/LabyrinthViewImplementer.java
@@ -11,10 +11,10 @@ public class LabyrinthViewImplementer implements LabyrinthView{
 
     public void getLabyrinth() {
         for(int i=0; i<this.size; i++){
-            for(int j=0; j<LabyrinthViewMatrix.length; j++){
+            for(int j=0; j<LabyrinthViewMatrix.length*2+1; j++){
                 System.out.print(LabyrinthViewMatrix[i].charAt(j));
             }
-            System.out.println("");
+            System.out.println(" ");
         }
 
     }
@@ -22,12 +22,13 @@ public class LabyrinthViewImplementer implements LabyrinthView{
     public void setLabyrinth(LabyrinthMatrixImpl model) {
         int[][] copy = model.getLabyrinth();
         for(int i=0; i<this.size; i++){
+            LabyrinthViewMatrix[i].append(Character.toString('|'));
             for(int j=0; j<this.size; j++){
-                if(LabyrinthViewMatrix[i] == null){
+                /*if(LabyrinthViewMatrix[i] == null){
                     LabyrinthViewMatrix[i].append(' ');
-                }
-                LabyrinthViewMatrix[i].append('|');
-                System.out.print(LabyrinthViewMatrix[i]);
+                }*/
+
+                //System.out.print(LabyrinthViewMatrix[i]);
                 char temp = 'x'; //x - caz invalid
 
                 if(copy[i][j] == -1){
@@ -39,12 +40,13 @@ public class LabyrinthViewImplementer implements LabyrinthView{
                 } else if (copy[i][j] == 2){
                     temp = 'F';
                 }
-                System.out.print(temp);
-                System.out.print(" ");
-
+               // System.out.print(temp);
+                //System.out.print(" ");
+                
                 LabyrinthViewMatrix[i].append(temp);
+               LabyrinthViewMatrix[i].append(Character.toString('|'));
             }
-            LabyrinthViewMatrix[i].append('|');
+
         }
     }
 
@@ -55,7 +57,11 @@ public class LabyrinthViewImplementer implements LabyrinthView{
     }
 
     LabyrinthViewImplementer(){
-        LabyrinthViewMatrix = new StringBuilder[size];
+        LabyrinthViewMatrix = new StringBuilder[size];//(this.size*2+1)
+
+        for (int i = 0; i < this.size; i++) {
+            LabyrinthViewMatrix[i] = new StringBuilder("");
+        }
 
 
     }
