48da53ffd53952ed2a5de38ff0b1793ffcd17608
xR86 <9412441+xR86@users.noreply.github.com>
2017-04-11 23:59:38 +0300

    updated android single screen app

--- Android-SingleScreen/app/src/main/java/io/github/xr86/singlescreen/MainActivity.java
+++ Android-SingleScreen/app/src/main/java/io/github/xr86/singlescreen/MainActivity.java
@@ -2,6 +2,10 @@ package io.github.xr86.singlescreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void coffeeAnimationTrigger(View view){
+        ImageView image = (ImageView) findViewById(R.id.coffee_icon);
+        Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        image.startAnimation(hyperspaceJump);
     }
 }
