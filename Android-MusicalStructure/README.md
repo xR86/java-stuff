9f24a8034a347272482905db0718f35e8f07d2f5
xR86 <9412441+xR86@users.noreply.github.com>
2017-07-08 23:59:24 +0300

    updated android musical structure app with basic play

---
+++ Android-MusicalStructure/README.md
@@ -0,0 +1,25 @@
+# java-stuff
+just java stuff ...
+
+## Musical structure app
+
+### App description
+Music player. (under development)
+
+### TODO
++ ...
+
+### App features
++ ...
+
+### App screens
+
+<img src="screen-main.png" alt="App screen" width="240" border="1" />
+> Main screen
+
+### Credits
+Sample tracks used: [incompetech.com/music/royalty-free/most/downloaded.php](http://incompetech.com/music/royalty-free/most/downloaded.php).
+  
+**The Complex Kevin MacLeod** (incompetech.com)  
+Licensed under Creative Commons: **By Attribution 3.0 License**  
+http://creativecommons.org/licenses/by/3.0/
--- Android-MusicalStructure/app/src/main/java/io/github/xr86/android_musicalstructure/MainActivity.java
+++ Android-MusicalStructure/app/src/main/java/io/github/xr86/android_musicalstructure/MainActivity.java
@@ -1,7 +1,14 @@
 package io.github.xr86.android_musicalstructure;
 
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupMediaPlayer();
+    }
+
+    private void setupMediaPlayer() {
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.the_complex);
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        String uriPath = "android.resource://" + getPackageName() + "/raw/" + "the_complex";
+        final Uri uri = Uri.parse(uriPath);
+        mmr.setDataSource(getApplication(), uri);
+
+        final String artistName =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_ARTIST);
+        final String trackName =
+                mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_TITLE);
+        //final byte[] trackArt =
+        //        mmr.getEmbeddedPicture();
+
+        final Button play = (Button) findViewById(R.id.play);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            boolean trackOpened = false;
+
+            public void onClick(View arg0) {
+                if (!trackOpened) {
+                    TextView trackTitle = (TextView) findViewById(R.id.track_title);
+                    if (artistName != "") {
+                        trackTitle.setText(artistName + " - " + trackName);
+                    } else {
+                        trackTitle.setText(getResources().getResourceEntryName(R.raw.the_complex));
+                    }
+                    trackOpened = true;
+                }
+                if (mp.isPlaying()) {
+                    mp.pause();
+                    play.setText("Start");
+                } else {
+                    mp.start();
+                    play.setText("Pause");
+                }
+            }
+        });
+    }
+
+    public void buttonClickPlaceholder(View v) {
+        Toast.makeText(this, "Feature not implemented !", Toast.LENGTH_SHORT).show();
     }
 }
