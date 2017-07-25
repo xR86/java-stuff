b54987b9d9690b83c7182f4806ad2eea8c1860e2
xR86 <9412441+xR86@users.noreply.github.com>
2017-07-25 23:59:37 +0300

    fixed android court counter app and small refactor

--- Android-CourtCounter/app/src/main/java/io/github/xr86/courtcounter/MainActivity.java
+++ Android-CourtCounter/app/src/main/java/io/github/xr86/courtcounter/MainActivity.java
@@ -8,7 +8,9 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,32 +27,34 @@ public class MainActivity extends AppCompatActivity {
 
         //set = (Button) findViewById(R.id.button4);
 
-        focus = (Chronometer) findViewById(R.id.chronometer1);
+        mChronometer = (Chronometer) findViewById(R.id.chronometer1);
     }
 
     protected void goalForTeamA(View view){
-        teamAGoals += 1;
-        TextView goals = (TextView) findViewById(R.id.team_a_score);
-        goals.setText(String.valueOf(teamAGoals));
+        if (isChronometerRunning) {
+            teamAGoals += 1;
+            TextView goals = (TextView) findViewById(R.id.team_a_score);
+            goals.setText(String.valueOf(teamAGoals));
 
+            ScrollView tempWrapper = (ScrollView) findViewById(R.id.goal_board_team_A_wrapper);
+            tempWrapper.measure(0, 0);
+            if (tempWrapper.getMeasuredHeight() > 400) {
+                LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT, 400);
+                tempWrapper.setLayoutParams(lp);
+            }
 
-        LinearLayout tempLayout = (LinearLayout) findViewById(R.id.goal_board_team_A);
+            LinearLayout tempLayout = (LinearLayout) findViewById(R.id.goal_board_team_A);
+            TextView tempTextView = (TextView)getLayoutInflater().inflate(R.layout.template_minute, null);
 
-        TextView tempTextView = new TextView(MainActivity.this);
+            String temp = "" + (SystemClock.elapsedRealtime() - mChronometer.getBase()) / 1000 / 60 + "'";//mChronometer.getText().toString();
+            tempTextView.setText(temp);
 
-        //TODO: problems here
-        ViewGroup.LayoutParams params =  new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        );
-//        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
-//        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
-//        tempTextView.setLayoutParams(params);
-//
-//        String temp = focus.getText().toString();
-//        tempTextView.setText(temp);
-//
-//        tempLayout.addView(tempTextView);
+            tempLayout.addView(tempTextView);
+        } else {
+            Toast toast = Toast.makeText(getApplicationContext(),"Please start the chronometer",Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 
     protected void goalForTeamB(View view){
@@ -60,26 +64,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*Timer functions*/
-    Chronometer focus;
+    Chronometer mChronometer;
+    boolean isChronometerRunning = false;
+    long timeWhenStopped = 0;
     Button start, stop, restart, set, clear;
 
     protected void clear(View v){
-        focus.setFormat(null);
+        mChronometer.setFormat(null);
     }
 
     protected void start(View v) {
-        focus.start();
+        isChronometerRunning = true;
+        mChronometer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+        mChronometer.start();
     }
 
     protected void stop(View v) {
-        focus.stop();
+        isChronometerRunning = false;
+        timeWhenStopped = mChronometer.getBase() - SystemClock.elapsedRealtime();
+        mChronometer.stop();
     }
 
     protected void restart(View v) {
-        focus.setBase(SystemClock.elapsedRealtime());
+        mChronometer.setBase(SystemClock.elapsedRealtime());
+        timeWhenStopped = 0;
     }
 
     protected void set(View v) {
-        focus.setFormat("Formated Time - %s");
+        mChronometer.setFormat("Formated Time - %s");
     }
 }
